c0148f0b2f237aad4f88e29969cf05a02bceecf5
Damith Nuwan Sampath <damithnuwans@gmail.com>
2015-11-14 11:29:41 +0530

    My first Coomit

---
+++ client/js/static/3Dcharts.js
@@ -0,0 +1,39 @@
+
+
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'column',
+            margin: 75,
+            options3d: {
+                enabled: true,
+                alpha: 10,
+                beta: 25,
+                depth: 70
+            }
+        },
+        title: {
+            text: 'Sales on last months'
+        },
+        subtitle: {
+            text: 'Values are displayed in 1000 scale'
+        },
+        plotOptions: {
+            column: {
+                depth: 25
+            }
+        },
+        xAxis: {
+            categories: Highcharts.getOptions().lang.shortMonths
+        },
+        yAxis: {
+            title: {
+                text: null
+            }
+        },
+        series: [{
+            name: 'Sales',
+            data: [2, 3, null, 4, 0, 5, 1, 4, 6, 3]
+        }]
+    });
+});
--- server.js
+++ server.js
@@ -18,6 +18,48 @@ app.get('/api/names',serverController.list);
 
 app.use(express.static('client'));
 
-app.listen(3000,function(){
-	console.log("I am listening");
+app.get('/financial',function(req,res){
+	//console.log(req);
+	res.sendFile(__dirname+'/client/views/main_menu_financial.html');
+});
+
+//pages for finacial department
+
+//app.use(express.static('client/views/fragments_financial'));
+
+app.get('/financial-p1',function(req,res){
+	res.sendFile(__dirname+'/client/views/fragments_financial/p1.html');
+});
+app.get('/financial-p2',function(req,res){
+	res.sendFile(__dirname+'/client/views/fragments_financial/p2.html');
+});
+app.get('/financial-p3',function(req,res){
+	res.sendFile(__dirname+'/client/views/fragments_financial/p3.html');
+});
+app.get('/financial-p4',function(req,res){
+	res.sendFile(__dirname+'/client/views/fragments_financial/p4.html');
+});
+app.get('/financial-p5',function(req,res){
+	res.sendFile(__dirname+'/client/views/fragments_financial/p5.html');
+});
+app.get('/financial-p6',function(req,res){
+	res.sendFile(__dirname+'/client/views/fragments_financial/p6.html');
+});
+app.get('/financial-p7',function(req,res){
+	res.sendFile(__dirname+'/client/views/fragments_financial/p7.html');
+});
+
+app.get('/financial-bRe',function(req,res){
+	res.sendFile(__dirname+'/client/views/fragments_financial/bankReconciliation.html'); 
+});
+
+app.get('/financial-bSt',function(req,res){
+	res.sendFile(__dirname+'/client/views/fragments_financial/statements.html'); 
+});
+
+//end of ajax pages
+
+
+app.listen(3001,function(){
+	console.log("I am listening port 3000");
 });
